3fcf2cae91f7f56824a983a8911591a62fb70529
zhanglei <zhanglei@miaotu.com>
2015-05-19 19:28:11 +0800

    张浩

--- MiaoTu/src/main/java/com/miaotu/activity/EditUserInfoActivity.java
+++ MiaoTu/src/main/java/com/miaotu/activity/EditUserInfoActivity.java
@@ -1,5 +1,6 @@
 package com.miaotu.activity;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,7 +8,7 @@ import android.view.MenuItem;
 
 import com.miaotu.R;
 
-public class EditUserInfoActivity extends ActionBarActivity {
+public class EditUserInfoActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,25 +16,4 @@ public class EditUserInfoActivity extends ActionBarActivity {
         setContentView(R.layout.activity_edit_user_info);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_edit_user_info, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
