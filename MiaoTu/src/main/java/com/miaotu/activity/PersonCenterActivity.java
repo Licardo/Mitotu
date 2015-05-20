d94c256b91e13f24a70ff15f31efb6bd639ad274
zhanglei <zhanglei@miaotu.com>
2015-05-20 17:49:54 +0800

    张浩

---
+++ MiaoTu/src/main/java/com/miaotu/activity/PersonCenterActivity.java
@@ -0,0 +1,19 @@
+package com.miaotu.activity;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.miaotu.R;
+
+public class PersonCenterActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person_center);
+    }
+
+}
